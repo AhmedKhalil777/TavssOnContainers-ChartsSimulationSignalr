60820186f7b88b15bf56d11f560c9e772c5d7686
AhmedKhalil777 <progeng_Ahmed_Khalil@outlook.com>
2020-02-05 04:26:11 +0200

    20200204{Signalr connection transfering commit }

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {SignalRService} from './services/signal-r.service';
+import {HttpClient} from '@angular/common/http';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  constructor(public signalRService : SignalRService ,  private Http :HttpClient){};
+
+  ngOnInit(){
+    this.signalRService.startConnection();
+    this.signalRService.addTransferChartDataListner();
+    this.startHttpRequest();
+  };
   title = 'ChartsSignalr';
+  private startHttpRequest = () => {
+    this.Http.get('https://localhost:5001/api/v1/Chart')
+             .subscribe(result => console.log( result));
+  };
 }
--- src/app/services/signal-r.service.ts
+++ src/app/services/signal-r.service.ts
@@ -6,6 +6,27 @@ import { ChartModel } from '../interfaces/chart-model'
   providedIn: 'root'
 })
 export class SignalRService {
+  public data : ChartModel[];
+  private hubConnection : signalr.HubConnection 
 
   constructor() { }
+
+  public startConnection = () => {
+    this.hubConnection = new signalr.HubConnectionBuilder()
+                                    .withUrl('https://localhost:5001/Charts')
+                                    .build();
+    
+    this.hubConnection
+        .start()
+        .then(() => console.log('The Connection started'))
+        .catch(err => console.log('There are an error :' + err)  );
+    
+  };
+
+  public addTransferChartDataListner =() => {
+    this.hubConnection.on('TransferCharts', (data) => {
+      this.data = data;
+      console.log(data); 
+    }); 
+  };
 }
